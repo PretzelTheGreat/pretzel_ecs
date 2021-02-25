9194defa037ee69c1f16994259d1c4a5d3994849
Christian Wetzel <chwetzel122116@gmail.com>
2021-02-24 21:45:57 -0500

    added documentation to the code

--- entity_manager.py
+++ entity_manager.py
@@ -2,16 +2,60 @@ from uuid import uuid4 as U4
 
 
 class EntityManager:
+    """
+    EntityManager() manages entities within a program. It is intended to be a
+    static class, with the class brought in by importing it and accessing it directly.
+
+    ...
+
+    Attributes
+    ----------
+    tracked_entities : dict
+        the dictionary that contains all the tracked entities with a dictionary containing
+        all the attached components.
+
+    Methods
+    -------
+    create_entity(data=None)
+        Creates a new entity. Can be initialized empty or including data
+
+    get_entity_data(entity_id)
+        Retrieves data stored for the entity_id given
+
+    attach_data(entity_id, data)
+        Attaches new data to entity_id and stores it
+
+    get_entities_with_data(data_comp)
+        Searches all entities to get the ones with the supplied data components    
+
+    """
+
     tracked_entities = {"master_entity": {}}
 
     @classmethod
     def create_entity(cls, data=None):
-        """Creates a new entity. Can be initialized empty or including data"""
+        """Creates a new entity to be tracked, and attaches any supplied data.
+
+        If the argument 'data' is not passed in, a new entity id is given (given as a uuid)
+
+        Parameters
+        ----------
+        data : dict, tuple, optional
+            the data that the new entity will be initalized (default is None)
+            if the data is given in a dict, the entity data will be set to the dictionary
+            supplied
+            or if the data is given as a tuple, it will create a new dict with the first entry
+            in the tuple as the key and the second as the value
+        """
+
         new_id = U4()
 
-        if data != None:
+        if data != None and type(data) == tuple:
             cls.tracked_entities[new_id] = {data[0]: data[1]}
             return new_id
+
+        elif data != None and type(data) == dict:
+            cls.tracked_entities[new_id] = data
 
         else:
             cls.tracked_entities[new_id] = {}
@@ -19,15 +63,34 @@ class EntityManager:
 
     @classmethod
     def get_entity_data(cls, entity_id):
+        """Gets the given entity's data that is being tracked.
+
+        Parameters
+        ----------
+        entity_id : str
+            the entity id to be looked up.
+        """
+
         if entity_id in cls.tracked_entities:
             return cls.tracked_entities[entity_id]
 
     @classmethod
     def attach_data(cls, entity_id, data):
-        """data must be in format (Name (Easiest if Classname), Object)"""
+        """Attaches new data to an existing entity
+
+        Parameters
+        ----------
+        entity_id : str
+            the entity id to attach the data to
+        data : tuple
+            the data to be attached. the data must be in the following format:
+                (Name of Component [best if it is the name of the class], Object)
+        """
+
         if entity_id in cls.tracked_entities:
             cls.tracked_entities[entity_id][data[0]] = data[1]
 
     @classmethod
     def get_entities_with_data(cls, data_comp):
+        """Returns entities with the supplied components"""
         return [x for x in cls.tracked_entities.keys() if len(set(data_comp).intersection(cls.tracked_entities[x].keys())) == len(data_comp)]
